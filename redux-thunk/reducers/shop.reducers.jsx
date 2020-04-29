c8784d45551db8565c38fdeec9b11212d93622ae
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-04-29 16:57:30 +0545

    Redux Thunk

--- redux-thunk/reducers/shop.reducers.jsx
+++ redux-thunk/reducers/shop.reducers.jsx
@@ -1,11 +1,17 @@
-import SHOP_DATA from '../../pages/shop/shop.data'
-
 const INITIAL_STATE = {
-  collections: SHOP_DATA,
+  collections: null,
+  isFetching: false,
+  error: undefined,
 }
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case 'FETCH_COLLECTIONS_START':
+      return { ...state, isFetching: true }
+    case 'FETCH_COLLECTIONS_SUCCESS':
+      return { ...state, isFetching: false, collections: action.payload }
+    case 'FETCH_COLLECTIONS_FAILURE':
+      return { ...state, isFetching: false, error: action.payload }
     default:
       return state
   }
